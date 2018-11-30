e557d56cafcfd51b8f4880bdad5e78fd8a98a8b5
Artem <gretskovart@gmail.com>
2018-11-30 23:45:56 +0400

    add greeting view

--- js/templates-modules/module-greeting.js
+++ js/templates-modules/module-greeting.js
@@ -1,34 +1,14 @@
-import getElementFromTemplate from './../render-element.js';
 import moduleRules from './module-rules.js';
+import GreetingView from './../view/view-greeting.js';
 
-const greetingTemplate = `
-  <section class="greeting central--blur">
-    <img class="greeting__logo" src="img/logo_ph-big.svg" width="201" height="89" alt="Pixel Hunter">
-    <div class="greeting__asterisk asterisk"><span class="visually-hidden">Я просто красивая звёздочка</span>*</div>
-    <div class="greeting__challenge">
-      <h3 class="greeting__challenge-title">Лучшие художники-фотореалисты бросают тебе вызов!</h3>
-      <p class="greeting__challenge-text">Правила игры просты:</p>
-      <ul class="greeting__challenge-list">
-        <li>Нужно отличить рисунок от фотографии и сделать выбор.</li>
-        <li>Задача кажется тривиальной, но не думай, что все так просто.</li>
-        <li>Фотореализм обманчив и коварен.</li>
-        <li>Помни, главное — смотреть очень внимательно.</li>
-      </ul>
-    </div>
-    <button class="greeting__continue" type="button">
-      <span class="visually-hidden">Продолжить</span>
-      <svg class="icon" width="64" height="64" viewBox="0 0 64 64" fill="#000000">
-        <use xlink:href="img/sprite.svg#arrow-right"></use>
-      </svg>
-    </button>
-  </section>
-`;
+const getGreetingScreen = () => {
+  const greeting = new GreetingView();
+  const greetingScreen = greeting.render();
 
-const changeScreenToRules = () => document.querySelector(`.greeting__continue`).addEventListener(`click`, moduleRules);
+  greeting.clickNextHandler = moduleRules;
+  greeting.bind();
 
-const moduleGreeting = () => {
-  getElementFromTemplate(greetingTemplate);
-  changeScreenToRules();
+  return greetingScreen;
 };
 
-export default moduleGreeting;
+export default getGreetingScreen;
---
+++ js/view/view-greeting.js
@@ -0,0 +1,37 @@
+import AbstractView from './abstract-view.js';
+
+class GreetingView extends AbstractView {
+  get template() {
+    return `
+      <section class="greeting central--blur">
+        <img class="greeting__logo" src="img/logo_ph-big.svg" width="201" height="89" alt="Pixel Hunter">
+        <div class="greeting__asterisk asterisk"><span class="visually-hidden">Я просто красивая звёздочка</span>*</div>
+        <div class="greeting__challenge">
+          <h3 class="greeting__challenge-title">Лучшие художники-фотореалисты бросают тебе вызов!</h3>
+          <p class="greeting__challenge-text">Правила игры просты:</p>
+          <ul class="greeting__challenge-list">
+            <li>Нужно отличить рисунок от фотографии и сделать выбор.</li>
+            <li>Задача кажется тривиальной, но не думай, что все так просто.</li>
+            <li>Фотореализм обманчив и коварен.</li>
+            <li>Помни, главное — смотреть очень внимательно.</li>
+          </ul>
+        </div>
+        <button class="greeting__continue" type="button">
+          <span class="visually-hidden">Продолжить</span>
+          <svg class="icon" width="64" height="64" viewBox="0 0 64 64" fill="#000000">
+            <use xlink:href="img/sprite.svg#arrow-right"></use>
+          </svg>
+        </button>
+      </section>
+  `;
+  }
+
+  clickNextHandler() {}
+
+  bind() {
+    document.querySelector(`.greeting__continue`).addEventListener(`click`, this.clickNextHandler);
+  }
+}
+
+export default GreetingView;
+
